dd728a5fd72fa9d9fdc83d90b0b2f979a2843555
Spiderfav <ruifavinha2002@gmail.com>
2018-10-05 15:17:56 +0100

    Update Song.py

--- Song.py
+++ Song.py
@@ -1,19 +1,48 @@
 import Auth_score
+import sys
+
+#from Auth_score import user
+
+#print(user)
+
 
 Auth_score.input_usr()
 
+print("Player 1:")
 from Auth_score import admin_usr
 
-while admin_usr != 1:
-    Auth_score.input_usr()
+if admin_usr == 1:
+    print("Go ahead, you are authorised.")
+    print(admin_usr)
+    #user = player1
 
-    if admin_usr == 1:
-        print("Go ahead, you are authorised.")
-        print(admin_usr)
-        break
-    else:
-        print("Not authorised to play.")
-        print(admin_usr)
-        print("")
+else:
+    print("Not authorised to play.")
+    print(admin_usr)
+    print("")
+    sys.exit()
 
-print("Does it work?")
+
+Auth_score.input_usr()
+
+print("Player 2:")
+from Auth_score import admin_usr
+
+if admin_usr == 1:
+    print("Go ahead, you are authorised.")
+    print(admin_usr)
+    #user = player2
+
+else:
+    print("Not authorised to play.")
+    print(admin_usr)
+    print("")
+    sys.exit()
+
+Songs = open("SongsAndArtists.txt", "r")
+Songs = Songs.read()
+# Splits into a dictionary using the character to differenciate
+items = Songs.split(',')
+item1 = items[0]
+item1.replace('a'+'e'+'i'+'o'+'u', '--')
+print(item1)
