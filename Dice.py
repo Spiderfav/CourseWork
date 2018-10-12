6926f1f5354020160fde75be0df154568c9f168a
Spiderfav <ruifavinha2002@gmail.com>
2018-10-12 15:20:38 +0100

    Completed most bugs

--- Dice.py
+++ Dice.py
@@ -1,55 +1,76 @@
 import random
 import time
 
-p1_dice = random.randint(1,6)
-p1_2_dice = random.randint(1,6)
+p1_wins = 0
 
-p2_dice = random.randint(1,6)
-p2_2_dice = random.randint(1,6)
+p2_wins = 0
 
-p1_total = p1_dice + p1_2_dice
-p2_total = p2_dice + p2_2_dice
+for x in range(1,5):
+   p1_dice = random.randint(1,6)
+   p1_2_dice = random.randint(1,6)
 
-print("Player 1 rolls...")
-time.sleep(2)
-print("Player 1 rolls a total of:",p1_total)
+   p2_dice = random.randint(1,6)
+   p2_2_dice = random.randint(1,6)
 
-if (p1_total % 2) == 0:
-   print("It's even! Add 10 points!")
-   p1_total2 = p1_total + 10
-else:
-   print("It's odd! Subtract 5 points!")
-   p1_total2 = p1_total - 5
+   p1_total = p1_dice + p1_2_dice
+   p2_total = p2_dice + p2_2_dice
 
-time.sleep(1)
+   print("Player 1 rolls...")
+   time.sleep(2)
+   print("Player 1 rolls a total of:",p1_total)
 
-print("Player 2 rolls...")
-time.sleep(2)
-print("Player 2 rolls a total of:",p2_total)
+   if (p1_total % 2) == 0:
+      print("It's even! Add 10 points!")
+      p1_total2 = p1_total + 10
+   else:
+      print("It's odd! Subtract 5 points!")
+      p1_total2 = p1_total - 5
 
-if (p2_total % 2) == 0:
-   print("It's even! Add 10 points!")
-   p2_total2 = p2_total + 10
-else:
-   print("It's odd! Subtract 5 points!")
-   p2_total2 = p2_total - 5
+   time.sleep(1)
 
+   print("Player 2 rolls...")
+   time.sleep(2)
+   print("Player 2 rolls a total of:",p2_total)
+
+   if (p2_total % 2) == 0:
+      print("It's even! Add 10 points!")
+      p2_total2 = p2_total + 10
+   else:
+      print("It's odd! Subtract 5 points!")
+      p2_total2 = p2_total - 5
+
+      
+   print(p1_total2)
+   print(p2_total2)
+
+   if p1_total2 < 0:
+      p1_total2 = 0
+
+   elif p2_total2 < 0:
+      p2_total2 = 0
+
+
+   if p1_total2 > p2_total2:
+      print("P1 wins!")
+      p1_wins = p1_wins + 1
+
+   elif p1_total2 == p2_total2:
+      print("It's a draw!")
+
+   else:
+      print("P2 wins!")
+      p2_wins = p2_wins + 1 
+
+   print("")
    
-print(p1_total2)
-print(p2_total2)
+print(p1_wins)
+print(p2_wins)
 
-if p1_total2 < 0:
-   p1_total2 = 0
+if p1_wins > p2_wins:
+   print("P1 wins the game!")
 
-elif p2_total2 < 0:
-   p2_total2 = 0
-
-
-if p1_total2 > p2_total2:
-   print("P1 wins!")
-
-elif p1_total2 == p2_total2:
-   print("It's a draw!")
+elif p1_wins == p2_wins:
+   print("Added")
 
 else:
-   print("P2 wins!")
+   print("P2 wins the gane!")
