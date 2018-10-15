2d8befb2838e5d118972232b5134a8016d62cc8d
Spiderfav <ruifavinha2002@gmail.com>
2018-10-15 12:14:12 +0100

    Should Work

--- Dice.py
+++ Dice.py
@@ -1,6 +1,21 @@
 import random
 import time
 
+def undicided ( p1_wins, p2_wins):
+    while p1_wins == p2_wins:
+      roll0 = random.randint(1,6)
+      roll1 = random.randint(1,6)
+      if roll0 > roll1:
+         print("P1 wins!")
+         p1_wins = p1_wins +1
+
+      elif roll0 == roll1:
+         undicided( p1_wins, p2_wins)
+
+      else:
+         print("P2 Wins!")
+         p2_wins = p2_wins +1
+
 p1_wins = 0
 
 p2_wins = 0
@@ -16,7 +31,7 @@ for x in range(1,5):
    p2_total = p2_dice + p2_2_dice
 
    print("Player 1 rolls...")
-   time.sleep(2)
+   #time.sleep(2)
    print("Player 1 rolls a total of:",p1_total)
 
    if (p1_total % 2) == 0:
@@ -26,10 +41,10 @@ for x in range(1,5):
       print("It's odd! Subtract 5 points!")
       p1_total2 = p1_total - 5
 
-   time.sleep(1)
+   #time.sleep(1)
 
    print("Player 2 rolls...")
-   time.sleep(2)
+   #time.sleep(2)
    print("Player 2 rolls a total of:",p2_total)
 
    if (p2_total % 2) == 0:
@@ -70,7 +85,9 @@ if p1_wins > p2_wins:
    print("P1 wins the game!")
 
 elif p1_wins == p2_wins:
-   print("Added")
+   print("Roll one dice to decide!")
+   undicided( p1_wins, p2_wins)
+  
 
 else:
    print("P2 wins the gane!")
