cbb8e9cc1f1122d26d88238a71007d5ceb59630a
Spiderfav <ruifavinha2002@gmail.com>
2018-10-16 10:00:43 +0100

    Update Song.py

--- Song.py
+++ Song.py
@@ -1,8 +1,23 @@
 list_char = "AEIOU"
-Songs = open("Admin.txt", "r")
+Songs = open("Songs.txt", "r")
+Artists = open("Artists.txt", "r")
 Songs = Songs.read()
 #Splits into a dictionary using the character to differenciate
 items = Songs.split(',')
-item1 = items[0]
-item1.replace('H','-')
-print(item1)
+print(items)
+for list_char in items:
+    items[1].append("-")
+    print(items)
+
+
+
+
+
+##item1 = str(items[0])
+##item1.replace('H','-')
+##print(item1)
+##mainStr = "Hello, This is a sample string"
+##otherStr = mainStr.replace('s' , 'X') 
+##print(otherStr)
+##for character in secretWord:
+##        guess_word.append("-")
