476b9197bcf851b65aaab2f6742f2980f5a696af
Spiderfav <ruifavinha2002@gmail.com>
2018-10-17 13:53:24 +0100

    Working Letter Remover

--- Song.py
+++ Song.py
@@ -1,23 +1,24 @@
-list_char = "AEIOU"
+#list_char = ["A","E","I","O","U"]
 Songs = open("Songs.txt", "r")
 Artists = open("Artists.txt", "r")
 Songs = Songs.read()
 #Splits into a dictionary using the character to differenciate
 items = Songs.split(',')
-print(items)
-for list_char in items:
-    items[1].append("-")
-    print(items)
+test = items[0].replace("A","-")
+print(test)
+
+test0 = test.replace("E","-")
+print(test0)
+
+test1 = test0.replace("I","-")
+print(test1)
+
+test2 = test1.replace("O","-")
+print(test2)
+
+test3 = test2.replace("U","-")
+print(test3)
 
 
 
 
-
-##item1 = str(items[0])
-##item1.replace('H','-')
-##print(item1)
-##mainStr = "Hello, This is a sample string"
-##otherStr = mainStr.replace('s' , 'X') 
-##print(otherStr)
-##for character in secretWord:
-##        guess_word.append("-")
