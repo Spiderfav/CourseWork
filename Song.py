01c865f95a1043b3dfd3c00c96255aae9f3390ab
Spiderfav <ruifavinha2002@gmail.com>
2018-10-17 14:11:35 +0100

    Deletes letters for songs and artists
    Probably need to put it as a function so it can be carried on for multiple songs and that way, can have a unlimited number of tries, if need be.

--- Song.py
+++ Song.py
@@ -1,23 +1,32 @@
 #list_char = ["A","E","I","O","U"]
+import random
+
+def remove():
+    test = items[i].replace("A","-")
+    #print(test)
+
+    test0 = test.replace("E","-")
+    #print(test0)
+
+    test1 = test0.replace("I","-")
+    #print(test1)
+
+    test2 = test1.replace("O","-")
+    #print(test2)
+
+    test3 = test2.replace("U","-")
+    print(test3)
+
+
 Songs = open("Songs.txt", "r")
 Artists = open("Artists.txt", "r")
 Songs = Songs.read()
-#Splits into a dictionary using the character to differenciate
 items = Songs.split(',')
-test = items[0].replace("A","-")
-print(test)
-
-test0 = test.replace("E","-")
-print(test0)
-
-test1 = test0.replace("I","-")
-print(test1)
-
-test2 = test1.replace("O","-")
-print(test2)
-
-test3 = test2.replace("U","-")
-print(test3)
+i = random.randint(0,1)
+remove()
+Artists = Artists.read()
+items = Artists.split(',')
+remove()
 
 
 
