83172288d3004406fffb03a6645e4bfb1d4a1102
Spiderfav <ruifavinha2002@gmail.com>
2018-10-19 14:32:23 +0100

    Comments

--- Dice.py
+++ Dice.py
@@ -1,7 +1,9 @@
 import random
 import time
 
+#This is used at a later date for undecided result
 def undicided ( p1_wins, p2_wins):
+    #Keeps looping while Player 1 wins is equal to player's 2 wins
     while p1_wins == p2_wins:
       roll0 = random.randint(1,6)
       roll1 = random.randint(1,6)
@@ -16,22 +18,27 @@ def undicided ( p1_wins, p2_wins):
          print("P2 Wins!")
          p2_wins = p2_wins +1
 
+#Setting both variables as zero for later
 p1_wins = 0
 
 p2_wins = 0
 
+#Repeat for 5 times for five games
 for x in range(1,5):
+   #Rolling Player 1's dice
    p1_dice = random.randint(1,6)
    p1_2_dice = random.randint(1,6)
 
+   #Rolling Player 2's dice
    p2_dice = random.randint(1,6)
    p2_2_dice = random.randint(1,6)
 
+   #Added their totals to be compared later
    p1_total = p1_dice + p1_2_dice
    p2_total = p2_dice + p2_2_dice
 
    print("Player 1 rolls...")
-   #time.sleep(2)
+   time.sleep(2)
    print("Player 1 rolls a total of:",p1_total)
 
    if (p1_total % 2) == 0:
@@ -41,10 +48,10 @@ for x in range(1,5):
       print("It's odd! Subtract 5 points!")
       p1_total2 = p1_total - 5
 
-   #time.sleep(1)
+   time.sleep(1)
 
    print("Player 2 rolls...")
-   #time.sleep(2)
+   time.sleep(2)
    print("Player 2 rolls a total of:",p2_total)
 
    if (p2_total % 2) == 0:
@@ -55,9 +62,10 @@ for x in range(1,5):
       p2_total2 = p2_total - 5
 
       
-   print(p1_total2)
-   print(p2_total2)
+   print("Player 1's total is:",p1_total2)
+   print("Player 2's total is:",p2_total2)
 
+   #If either total falls bellow 0
    if p1_total2 < 0:
       p1_total2 = 0
 
@@ -65,6 +73,7 @@ for x in range(1,5):
       p2_total2 = 0
 
 
+   #This is to determine which Player wins
    if p1_total2 > p2_total2:
       print("P1 wins!")
       p1_wins = p1_wins + 1
@@ -78,8 +87,8 @@ for x in range(1,5):
 
    print("")
    
-print(p1_wins)
-print(p2_wins)
+print("Player 1 has won",p1_wins,"game/s!")
+print("Player 2 has won",p2_wins,"game/s!")
 
 if p1_wins > p2_wins:
    print("P1 wins the game!")
